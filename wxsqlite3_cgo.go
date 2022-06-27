b873328726c70909ac20664588781a99bfdeb2a0
ahj <ahjcode@outlook.com>
2022-06-27 17:42:11 +0800

    fix: 打32位包时不使用-msse4.1 -maes等参数

--- wxsqlite3_cgo.go
+++ wxsqlite3_cgo.go
@@ -1,8 +1,7 @@
 package sqlite3
 
 /*
-#cgo amd64 CFLAGS: -msse4.1
-#cgo !arm64 CFLAGS: -maes
+#cgo amd64 CFLAGS: -msse4.1 -maes
 #cgo CFLAGS: -DUSE_LIBSQLITE3 -DCODEC_TYPE=CODEC_TYPE_RC4 -DSQLITE_HAS_CODEC=1
 */
 import "C"
